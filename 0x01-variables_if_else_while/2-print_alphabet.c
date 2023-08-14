dd7d2083c25975d2c3680f3ce8540cd467ceb4a7
RawaaSaif21 <saifeldeenrawaa@gmail.com>
2023-08-14 14:57:25 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *
+ *Return: 0 when successful
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
