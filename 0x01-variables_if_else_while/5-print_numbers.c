c5bc5870b3686c2287953253b9bfef12f1f29f35
RawaaSaif21 <saifeldeenrawaa@gmail.com>
2023-08-14 16:15:58 -0700

    5.Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,12 @@
 
 int main(void)
 {
-        int n;
+	int n;
 
-        for (n = 0; n <= 9; n++)
-        {
-                printf("%d", n);
-        }
-        printf("\n");
-        return (0);
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
 }
